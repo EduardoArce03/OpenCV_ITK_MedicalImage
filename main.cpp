b54cbec33dec24e065d9b407e979a0f8d967a40f
Andres0918 <chablaandres@gmail.com>
2025-04-02 10:42:22 -0500

    Commir_Prueba

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Main {
+    public:
+    static void run(){
+        std:: cout << "HOLA BICHO EDUALDO!!! XD" << std::endl;
+    }
+};
+
+int main(){
+    Main::run();
+    return 0;
+}
