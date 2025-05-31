75645a262f9555d5e6205d10b4e2f87f4cee4713
eduardo <eduardomaldonado2003ortiz@gmail.com>
2025-05-30 20:11:41 -0500

    implementacion de coloreo en segmentos de tumor

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,98 +1,45 @@
 #include <opencv2/opencv.hpp>
-
 #include "brats_loader.h"
-#include "itkOpenCVImageBridge.h"
-#include "itkRescaleIntensityImageFilter.h"
-#include "itkImage.h"
-#include "itkIndex.h"
+#include "nii_processing.h"  // Aquí están processSlice, extractMaskSlice, overlayMask
 
 using namespace cv;
 using namespace std;
-using namespace itk;
 
 int main(int argc, char *argv[]) {
     std::string path = "../dataset/HGG/BraTS19_2013_2_1";
 
     BratsData patient = loadBratsCase(path);
     auto flair = patient.modalities["flair"];
+    auto seg = patient.segmentation;
 
-    // Imprimir dimensiones del volumen
     auto region = flair->GetLargestPossibleRegion();
     auto size = region.GetSize();
 
-    cout << "Dimensiones del volumen:" << endl;
-    cout << "  X: " << size[0] << endl;
-    cout << "  Y: " << size[1] << endl;
-    cout << "  Z: " << size[2] << endl;
+    cout << "Dimensiones del volumen: "
+         << size[0] << " x " << size[1] << " x " << size[2] << endl;
 
     if (size[2] == 1) {
-        cout << "La imagen es 2D, solo tiene un slice en Z." << endl;
+        cout << "Imagen 2D con un solo slice." << endl;
     } else {
-        cout << "La imagen es 3D, con " << size[2] << " slices en Z." << endl;
+        cout << "Imagen 3D con " << size[2] << " slices." << endl;
     }
 
-    // Ahora puedes continuar con el resto del código para extraer el slice, etc.
-    int z = size[2] / 2;
+    int sliceIndex = 123; // slice medio
 
-    using PixelType = float;
-    constexpr unsigned int Dimension2D = 2;
-    using SliceType = Image<PixelType, Dimension2D>;
+    // Procesar slice para obtener imagen y máscara procesada
+    Mat imgSlice = processSlice(flair, sliceIndex);          // Imagen procesada (gradiente + máscara)
+    Mat maskSlice = extractMaskSlice(seg, sliceIndex);       // Máscara original (segmentación manual)
+    threshold(maskSlice, maskSlice, 0, 255, THRESH_BINARY);
 
-    SliceType::Pointer slice = SliceType::New();
+    // Superponer máscara procesada sobre imagen
+    Mat overlay = overlayMask(imgSlice, maskSlice, 0.4);
 
-    SliceType::SizeType size2D = {size[0], size[1]};
-    SliceType::IndexType start = {0, 0};
+    // Mostrar resultados
+    imshow("Slice procesado (Z=" + to_string(sliceIndex) + ")", imgSlice);
+    imshow("Máscara original (Z=" + to_string(sliceIndex) + ")", maskSlice);
+    imshow("Superposición máscara", overlay);
 
-    SliceType::RegionType sliceRegion;
-    sliceRegion.SetIndex(start);
-    sliceRegion.SetSize(size2D);
-    slice->SetRegions(sliceRegion);
-    slice->Allocate();
-
-    for (int y = 0; y < static_cast<int>(size2D[1]); ++y) {
-        for (int x = 0; x < static_cast<int>(size2D[0]); ++x) {
-            Index<3> idx3D = {x, y, z};
-            Index<2> idx2D = {x, y};
-            slice->SetPixel(idx2D, flair->GetPixel(idx3D));
-        }
-    }
-
-    using UCharImage2D = Image<unsigned char, Dimension2D>;
-    auto rescaler = RescaleIntensityImageFilter<SliceType, UCharImage2D>::New();
-    rescaler->SetInput(slice);
-    rescaler->SetOutputMinimum(0);
-    rescaler->SetOutputMaximum(255);
-    rescaler->Update();
-
-    Mat img = itk::OpenCVImageBridge::ITKImageToCVMat(rescaler->GetOutput());
-    imshow("Slice Z=" + std::to_string(z), img);
     waitKey(0);
-    auto maxZ = size[2];
-    for (unsigned int sliceIdx = 0; sliceIdx < maxZ; sliceIdx += 20) {
-        // Extraer slice actual
-        SliceType::Pointer slice = SliceType::New();
-        slice->SetRegions(sliceRegion);
-        slice->Allocate();
-
-        for (int y = 0; y < static_cast<int>(size2D[1]); ++y) {
-            for (int x = 0; x < static_cast<int>(size2D[0]); ++x) {
-                Index<3> idx3D = {x, y, sliceIdx};
-                Index<2> idx2D = {x, y};
-                slice->SetPixel(idx2D, flair->GetPixel(idx3D));
-            }
-        }
-
-        auto rescaler = RescaleIntensityImageFilter<SliceType, UCharImage2D>::New();
-        rescaler->SetInput(slice);
-        rescaler->SetOutputMinimum(0);
-        rescaler->SetOutputMaximum(255);
-        rescaler->Update();
-
-        Mat img = itk::OpenCVImageBridge::ITKImageToCVMat(rescaler->GetOutput());
-        imshow("Slice Z=" + std::to_string(sliceIdx), img);
-        cout << "Mostrando slice Z=" << sliceIdx << endl;
-        waitKey(0);
-    }
 
     return 0;
 }
--- include/nii_processing.h
+++ include/nii_processing.h
@@ -12,4 +12,6 @@ using namespace cv;
 
 using SliceType = Image<float,2>;
 Mat processSlice(Image<float, 3>::Pointer volume, unsigned int sliceIndex);
+Mat extractMaskSlice(itk::Image<unsigned char, 3>::Pointer maskVolume, unsigned int sliceIndex);
+Mat overlayMask(const Mat& image, const Mat& mask, double alpha=0.4);
 #endif //NII_PROCESSING_H
